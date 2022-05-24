0e076c725e4ddc5ffb944235e59a6cf25f158730
jacky <3474660038@qq.com>
2022-05-24 19:30:12 +0800

    Removed some code

--- Pygame.py
+++ Pygame.py
@@ -1,19 +1,4 @@
-import math
-import os
-import sys
-from lib2to3.pgen2.token import GREATER
-from urllib import request
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
